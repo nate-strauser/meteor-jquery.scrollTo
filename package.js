9e0c4ca1fd1e99f95363365eeb1690be7b6da8f0
Nathan Strauser <nathan.strauser@gmail.com>
2013-02-18 17:36:36 -0500

    atmos files

---
+++ package.js
@@ -0,0 +1,8 @@
+Package.describe({
+    summary: "jquery.scrollTo pacakged for meteor"
+});
+
+Package.on_use(function (api) {
+    api.use('jquery', 'client');
+    api.add_files("lib/jquery.scrollTo/jquery.scrollTo.js", "client");
+});
