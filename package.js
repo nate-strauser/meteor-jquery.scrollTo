ab6f05c43e2a54fc7711e5a2ff8daa45e5ff4322
Nathan Strauser <nathan.strauser@gmail.com>
2013-08-22 14:59:28 -0400

    version bump

--- package.js
+++ package.js
@@ -4,5 +4,5 @@ Package.describe({
 
 Package.on_use(function (api) {
     api.use('jquery', 'client');
-    api.add_files("lib/jquery.scrollTo/jquery.scrollTo.js", "client");
+    api.add_files("lib/jquery.scrollTo/jquery.scrollTo.js", "client", {bare:true});
 });
